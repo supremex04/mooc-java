7b2f3ef0e42780209d60a3da838ed4b69925674e
Supreme Bhandari <bhandarisupreme7@gmail.com>
2025-06-28 14:23:18 +0545

    Add files via upload

---
+++ part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class AverageOfThreeNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int x = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int y = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the third number:");
+        int p = Integer.valueOf(scanner.nextLine());
+        double z = ((double)(x+y+p)) / 3;
+        System.out.println("The average is " + z);
+
+        // Write your program here
+
+    }
+}
---
+++ part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class SpeedingTicket {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give speed:");
+        int x = Integer.valueOf(scanner.nextLine());
+        if ( x > 120){
+            System.out.println("Speeding ticket!");
+        }
+        // Write your program here. 
+    }
+}
