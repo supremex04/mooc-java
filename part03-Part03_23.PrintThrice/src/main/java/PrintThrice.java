848fdd04fa6ba84cae2d109d184c13298af19ce9
Supreme Bhandari <bhandarisupreme7@gmail.com>
2025-07-01 14:31:12 +0545

    part 3 -24

---
+++ part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class PrintThrice {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+        System.out.println("Give a word: ");
+        String word = scanner.nextLine();
+        word = word+word+word;
+        System.out.println(word);
+
+    }
+}
